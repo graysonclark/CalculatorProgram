ba76a421371b2dec4e2ac4596c7c12c048bc5338
Grayson Clark <gpc7@duke.edu>
2019-09-09 12:50:56 -0400

    Added LDL function to the calculator

--- calculator.py
+++ calculator.py
@@ -1,7 +1,17 @@
+def check_LDL(LDL_result):
+    if LDL_result < 130:
+        return "Normal"
+    elif 130 <= LDL_result < 159:
+        return "Borderline high"
+    elif 160 < LDL_result <= 189:
+        return "High"
+    elif LDL_result > 190:
+        return "Very High"
+
 def check_HDL(HDL_result):
     if HDL_result >= 60:
         return "Normal"
-    elif 40 <= HDL < 60:
+    elif 40 <= HDL_result < 60:
         return "Borderline low"
     else:
         return "low"
@@ -11,17 +21,24 @@ def cholesterol_interface():
     chol_input = input("Enter your cholesterol test results: ")
     chol_data = chol_input.split("=")
     if chol_data[0] == "HDL":
-        result = check_HDL(chol_data[1])
+        result = check_HDL(int(chol_data[1]))
         print("The result is {}".format(result))
+    elif chol_data[0] == "LDL":
+        result2 = check_LDL(int(chol_data[1]))
+        print("The result is {}".format(result2))
+
 def interface():
     print("My calculator program")
     keep_running = True
     while keep_running:
         print("Option: ")
+        print("1 - Cholesterol Check")
         print("9 - Quit")
         choice = input("Enter your choice: ")
         if choice == '9':
             keep_running = False
+        elif choice == '1':
+            cholesterol_interface()
     return
 
 if __name__ == "__main__":
